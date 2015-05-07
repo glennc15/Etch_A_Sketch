27e00cf73ec2c1b5d1c91ec839474c0b185d6d75
Glenn <gcrosby15@email.com>
2015-05-07 12:29:35 +0700

    Add javascript for the Erase button.  This required moving all square event handlers out of .ready() and into their own functions. All square event handlers are now set in addBoard().

--- js/etch.js
+++ js/etch.js
@@ -54,6 +54,39 @@ var setBlockDimensions = function(blockPerRow) {
    	blocks.width(blockDimensions);
 }
 
+var paintNow = false;
+// default color is red.
+var currentColor = 'red';
+
+var blockMousedownHandler = function () {
+	paintNow = true;
+}
+
+var blockMouseupHandler = function () {
+	paintNow = false;
+}
+
+var blockMouseenterHandler = function () {
+	// paint the tile the selected color if the mouse button is pressed.
+	if (paintNow) {
+		// Set the opacity. If a square has not been painted before it's
+		// initial opacity is 1.0 so set the the opacity to 0.2.  If the
+		// square has been painted then increase it's opacity by 0.2. Once a
+		// painted square's opacity is 1.0 do not change it's opacity agian.
+		var currentOpacity = +$(this).css('opacity');
+		if ($(this).hasClass('painted') && currentOpacity < 1.0) {
+			currentOpacity += 0.2;				
+		}
+		else if (!$(this).hasClass('painted')){
+			currentOpacity = 0.2;
+		}
+		currentOpacity = currentOpacity.toString();
+
+		// set the square's color, opacity and add the 'painted' class.
+		$(this).css({'background-color': currentColor, 'opacity': currentOpacity});
+		$(this).addClass('painted');
+	}
+};
 
 // Adds an etch-a-sketch board to the DOM. 
 var addBoard = function(numOfRows, numOfCols) {
@@ -67,67 +100,35 @@ var addBoard = function(numOfRows, numOfCols) {
 	var thisBoard = getBoard(numOfRows);
 	$('tbody').append($(thisBoard));
 
+	// Add event handlers for painting to the board squares.
+	$('.block').on('mousedown', blockMousedownHandler);
+	$('.block').on('mouseup', blockMouseupHandler);
+	$('.block').on('mouseenter', blockMouseenterHandler);
+
+	// Adjust the board block dimensions so the board fits on the screen.
 	setBlockDimensions(numOfRows);
 }
 
 
-
-var paintNow = false;
-// default color is red.
-var currentColor = 'red';
-
-
 $(document).ready(function() {
+	// Add a board with 16 squares
 	addBoard(16);
-
-	$('div').on('mousedown', function() {
-		paintNow = true;
-	});
-
-	$('div').on('mouseup', function() {
-		paintNow = false;
-	});
-
-	$('.block').on('mouseenter', function() {
-		// Original code to change the color of a tile.
-		// if (paintNow && !$(this).hasClass('makered')) {
-		// 	$(this).toggleClass('makered');
-		// }
-
-		// paint the tile the selected color if the mouse button is pressed.
-		if (paintNow) {
-			// Set the opacity. The opacity initially starts out at 0.2 and
-			// increases 0.2 each time the tile is recolored. The opacity of
-			// an initial, unpainted square is 1.0.
-			var currentOpacity = +$(this).css('opacity');
-
-			// if a square has not been painted before it's initial opacity is
-			// 1.0 so set the the opacity to 0.2.  If the square has been
-			// painted then increase it's opacity by 0.2. Once a painted
-			// square's opacity is 1.0 do not change it's opacity agian.
-			if ($(this).hasClass('painted') && currentOpacity < 1.0) {
-				currentOpacity += 0.2;				
-			}
-
-			else if (!$(this).hasClass('painted')){
-				currentOpacity = 0.2;
-			}
-			currentOpacity = currentOpacity.toString();
-
-			// set the square's color, opacity and add the 'painted' class.
-			$(this).css({'background-color': currentColor, 'opacity': currentOpacity});
-			$(this).addClass('painted');
-		}
-	});
-
+	
 
 	$('.button').click(function() {
-		var buttons = $('.button');
-		buttons.removeClass('selected');
-		$(this).addClass('selected');
+		// // highlights the current button.
+		// var buttons = $('.button');
+		// buttons.removeClass('selected');
+		// $(this).addClass('selected');
 
 		// set the color if a color select button was pressed.
 		if ($(this).hasClass('color_select')) {
+			// highlight the currently selected color button.
+			$('.color_select').removeClass('selected');
+			$(this).addClass('selected');
+
+			// determine which button was pressed and set the color property
+			// for painting.
 			switch($(this).attr('id')) {
 				case 'red':
 					currentColor = 'red';
@@ -147,8 +148,21 @@ $(document).ready(function() {
 			}
 		}
 
+		// Handle the control buttons.
+		if ($(this).hasClass('control')) {
+			switch($(this).attr('id')) {
+				case 'erase':
+					// Erase button: ask the user to enter how many grids to
+					// make the board. Then generates a new board with the
+					// number of grids specified by the user.
+					var boardSize = +prompt("What size of board do you want to draw on?", "50");
+					addBoard(boardSize);
+					break;
 
+				case 'help':
+					alert("Help! button clicked.");
+					break;
+			}
+		}
 	});
-    
-
 });
